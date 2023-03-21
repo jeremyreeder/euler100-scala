dc7dcc440dfa0d27a8b8585832f8f32e8b17007c
Jeremy Reeder <jeremy.reeder@pm.me>
2023-03-21 16:01:11 -0600

    Solved problem 69.

--- build.sbt
+++ build.sbt
@@ -10,5 +10,8 @@ lazy val root = (project in file("."))
 libraryDependencies ++=
 	Seq(
 		"org.scala-lang.modules" %% "scala-parallel-collections" % "1.0.4",
-		"org.scalatest" %% "scalatest" % "3.2.15" % "test"
+		"org.scalatest" %% "scalatest" % "3.2.15" % "test",
+		"org.scalaz" %% "scalaz-core" % "7.3.7"
 	)
+
+Test / parallelExecution := true
--- src/main/scala/Problem069.scala
+++ src/main/scala/Problem069.scala
@@ -1,24 +1,56 @@
 import Math.sqrt
 import scala.annotation.tailrec
 import scala.collection.mutable
+import scalaz.Memo.mutableHashMapMemo
 
 object Problem069 extends App {
-	@tailrec
-	private def greatestCommonDivisor(a: Int, b: Int): Int = // Euclidean algorithm
-		if b == 0 then a else greatestCommonDivisor(b, a % b)
 	
-	def areCoprimes(a: Int, b: Int) =
+	val Limit = 1_000_000
+	
+	def isPrime(n: BigInt) = n.isProbablePrime(certainty = 8)
+	
+	val primes = 2 :: (3 to Limit by 2).filter(isPrime).toList
+	
+	val primeFactors: Int => List[Int] = mutableHashMapMemo { n =>
+		@tailrec
+		def loop(n: Int, accumulator: List[Int]): List[Int] =
+			if isPrime(n) then return n :: accumulator
+			if n < 2 then return accumulator
+			primes.takeWhile(_ <= sqrt(n)).find(n % _ == 0) match {
+				case Some(p) => loop(n / p, p :: accumulator)
+				case None => accumulator
+			}
+		
+		loop(n, List.empty)
+	}
+ 
+	def areCoprime(a: Int, b: Int) = {
+		@tailrec
+		def greatestCommonDivisor(a: Int, b: Int): Int =
+			if b == 0 then a
+			else greatestCommonDivisor(b, a % b)
+		
 		greatestCommonDivisor(a, b) == 1
+	}
 	
-	def phi(n: Int) = (1 until n).count(areCoprimes(n, _))
+	val phi: Int => Int = mutableHashMapMemo {
+		case n if n < 2 => 0
+		case n if isPrime(n) => n - 1
+		case n =>
+			val factors = primeFactors(n)
+			if factors.distinct.size == factors.size then
+				factors.map(phi(_)).product
+			else (1 until n).count(areCoprime(_, n))
+	}
 	
 	def nToPhiRatio(n: Int) = n.toFloat / phi(n)
 	
-	val startTime = System.currentTimeMillis()
-	val answer = (2 to 100_000) maxBy nToPhiRatio
-	println(System.currentTimeMillis() - startTime)
-	println(answer)
+	// The value of phi(a*b) is minimized when a and b are prime. Additional factors can be included, e.g. phi(a*b*c).
+	// Let's consider only values of n which are the product of two or more distinct primes. Whenever there are
+	// duplicate prime factors, n does becomes larger but phi becomes proportionately larger as well, so there's no
+	// increase in the resulting ratio.
+	val candidates = (2 to Limit).map(primeFactors(_).distinct.product).distinct
 	
-	// TODO: Make it faster. This has yet to complete, so I need a more efficient approach.
-	// Eureka! The phi function is multiplicative, which means that phi(m * n) = phi(m) * phi(n) whenever gcd(m, n) = 1.
+	val answer = candidates.maxBy(nToPhiRatio)
+	println(answer)
 }
