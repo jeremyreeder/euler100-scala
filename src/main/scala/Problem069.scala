c89c294a1266e91974a038d8a2061b1daeafde56
Jeremy Reeder <jeremy.reeder@pm.me>
2023-03-21 20:00:36 -0600

    Simplified problem-69 solution, greatly improving performance.

--- src/main/scala/Problem069.scala
+++ src/main/scala/Problem069.scala
@@ -1,56 +1,24 @@
-import Math.sqrt
-import scala.annotation.tailrec
-import scala.collection.mutable
-import scalaz.Memo.mutableHashMapMemo
+import util.control.Breaks.{break, breakable}
 
 object Problem069 extends App {
 	
-	val Limit = 1_000_000
-	
-	def isPrime(n: BigInt) = n.isProbablePrime(certainty = 8)
-	
-	val primes = 2 :: (3 to Limit by 2).filter(isPrime).toList
-	
-	val primeFactors: Int => List[Int] = mutableHashMapMemo { n =>
-		@tailrec
-		def loop(n: Int, accumulator: List[Int]): List[Int] =
-			if isPrime(n) then return n :: accumulator
-			if n < 2 then return accumulator
-			primes.takeWhile(_ <= sqrt(n)).find(n % _ == 0) match {
-				case Some(p) => loop(n / p, p :: accumulator)
-				case None => accumulator
-			}
+	val primes =  {
+		def isPrime(n: BigInt) = n.isProbablePrime(certainty = 8)
 		
-		loop(n, List.empty)
-	}
- 
-	def areCoprime(a: Int, b: Int) = {
-		@tailrec
-		def greatestCommonDivisor(a: Int, b: Int): Int =
-			if b == 0 then a
-			else greatestCommonDivisor(b, a % b)
-		
-		greatestCommonDivisor(a, b) == 1
+		2 #:: LazyList.range(3, Int.MaxValue, step = 2).filter(isPrime)
 	}
 	
-	val phi: Int => Int = mutableHashMapMemo {
-		case n if n < 2 => 0
-		case n if isPrime(n) => n - 1
-		case n =>
-			val factors = primeFactors(n)
-			if factors.distinct.size == factors.size then
-				factors.map(phi(_)).product
-			else (1 until n).count(areCoprime(_, n))
+	// The ratio of n to phi(n) peaks ever-increasingly each time n reaches the product of a series of the smallest
+	// primes possible, so the answer is the product of the longest series of primes whose product doesn't exceed the
+	// limit, as follows.
+	
+	var answer = primes.head
+	var product = answer
+	breakable {
+		for n <- primes.tail do
+			product *= n
+			if product > 1_000_000 then break
+			answer = product
 	}
-	
-	def nToPhiRatio(n: Int) = n.toFloat / phi(n)
-	
-	// The value of phi(a*b) is minimized when a and b are prime. Additional factors can be included, e.g. phi(a*b*c).
-	// Let's consider only values of n which are the product of two or more distinct primes. Whenever there are
-	// duplicate prime factors, n does becomes larger but phi becomes proportionately larger as well, so there's no
-	// increase in the resulting ratio.
-	val candidates = (2 to Limit).map(primeFactors(_).distinct.product).distinct
-	
-	val answer = candidates.maxBy(nToPhiRatio)
 	println(answer)
 }
