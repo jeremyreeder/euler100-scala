cf0ade1762e49d607336c48b0d0c85dc2ddc66f4
Jeremy Reeder <jeremy.reeder@pm.me>
2023-03-24 11:00:24 -0600

    Solved problem 76

---
+++ src/main/scala/Problem076.scala
@@ -0,0 +1,29 @@
+/* Problem 76: Counting Summations
+It is possible to write five as a sum in exactly six different ways:
+	4 + 1
+	3 + 2
+	3 + 1 + 1
+	2 + 2 + 1
+	2 + 1 + 1 + 1
+	1 + 1 + 1 + 1 + 1
+
+How many different ways can one hundred be written as a sum of at least two positive integers?
+*/
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+object Problem076 extends App {
+	def countSummations(sum: Int): Long = {
+		val memo = mutable.Map[(Int, Int), Long]()
+		
+		def count(sum: Int, summandMax: Int): Long = {
+			if (sum == 0) 1
+			else if (summandMax > sum) 0
+			else memo.getOrElseUpdate((sum, summandMax), count(sum - summandMax, summandMax) + count(sum, summandMax + 1))
+		}
+		
+		count(sum, 1) - 1
+	}
+	
+	println(countSummations(100))
+}
