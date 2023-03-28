d4bb5af9dd63a65cc32f3742b277683fd7c21771
Jeremy Reeder <jeremy.reeder@pm.me>
2023-03-28 11:22:26 -0600

    Solved problem 83

--- src/main/scala/Problem083.scala
+++ src/main/scala/Problem083.scala
@@ -10,31 +10,47 @@ down, is indicated and is equal to 2297.
 Find the minimal path sum from the top left to the bottom right by moving left, right, up, and down in matrix.txt, a 31K
 text file containing an 80 by 80 matrix.
 */
+import scala.io.Source
+import util.control.Breaks.{break, breakable}
 
 object Problem083 extends App {
-	val matrix = // TODO: replace example matrix with file contents
-		List(
-			List(131, 673, 234, 103,  18),
-			List(201,  96, 342, 965, 150),
-			List(630, 803, 746, 422, 111),
-			List(573, 699, 497, 121, 956),
-			List(805, 732, 524,  37, 331)
-		)
+	val file = Source.fromFile("p082_matrix.txt")
+	val matrix = file.getLines.map(_.split(",").map(_.toInt).toList).toList
+	file.close()
 	
-	val (rows, columns) = (matrix.indices, matrix.head.indices)
+	// Bell-Fordman algorithm, with early exit
+	def minimalPathSum(matrix: List[List[Int]]): Int = {
+		val (rows, columns) = (matrix.indices, matrix.head.indices)
+		val distance = Array.tabulate(rows.size, columns.size) { (row, column) =>
+			if row == 0 && column == 0 then
+				matrix(row)(column)
+			else Int.MaxValue
+		}
+		breakable {
+			while true do {
+				var isPathFreshlyShortened = false
+				for {
+					rowA <- rows
+					columnA <- columns
+					if distance(rowA)(columnA) != Int.MaxValue
+					(rowB, columnB) <- List(
+						(rowA - 1, columnA), // up
+						(rowA + 1, columnA), // down
+						(rowA, columnA - 1), // left
+						(rowA, columnA + 1)  // right
+					)
+					if (rows contains rowB) && (columns contains columnB)
+				} do
+					val possiblyShorterDistance = distance(rowA)(columnA) + matrix(rowB)(columnB)
+					if possiblyShorterDistance < distance(rowB)(columnB) then
+						distance(rowB)(columnB) = possiblyShorterDistance
+						isPathFreshlyShortened = true
+				if !isPathFreshlyShortened then break
+			}
+		}
+		distance(rows.last)(columns.last)
+	}
 	
-	def minimumSumToEnd(row: Int, column: Int): Int =
-		if row == rows.last && column == columns.last then
-			return matrix(row)(column)
-		if !(rows contains row) || !(columns contains column) then
-			return Int.MaxValue
-		List(
-			minimumSumToEnd(row, column - 1), // left
-			minimumSumToEnd(row, column + 1), // right
-			minimumSumToEnd(row - 1, column), // up
-			minimumSumToEnd(row + 1, column)  // down
-		).min
-		
-	val answer = minimumSumToEnd(0, 0)
+	val answer = minimalPathSum(matrix)
 	println(answer)
 }
