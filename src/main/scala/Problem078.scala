23f81c3d41e14032a2514a52b5ecf2a9e44142ab
Jeremy Reeder <jeremy.reeder@pm.me>
2023-04-04 13:54:15 -0600

    Now using pentagonal-number theorem for problem 78. Still too slow.

--- src/main/scala/Problem078.scala
+++ src/main/scala/Problem078.scala
@@ -11,28 +11,31 @@ OO   O   O   O
 O   O   O   O   O
 Find the least value of n for which p(n) is divisible by one million.
 */
-import collection.mutable
-
 object Problem078 extends App {
-	def coinPartitionCounts(sum: Long) = {
-		val memo = mutable.Map[(Long, Long), Long]()
+	
+	def partition(n: Int): BigInt = {
+		val cache = Array.fill[BigInt](n + 1)(-1)
 		
-		def count(sum: Long, summandMax: Long): Long = {
-			if sum == 0 then 1L
-			else if summandMax > sum then 0L
+		def loop(m: Int): BigInt = {
+			if m < 0 then 0
+			else if m == 0 then 1
+			else if cache(m) != -1 then cache(m)
 			else
-				memo.get((sum, summandMax)) match {
-					case Some(result) => result
-					case None =>
-						val result = count(sum - summandMax, summandMax) + count(sum, summandMax + 1)
-						memo((sum, summandMax)) = result
-						result
-				}
+				var sum = BigInt(0)
+				var k = 1
+				while (true) do
+					val pentagonal1 = k * (3 * k - 1) / 2
+					val pentagonal2 = k * (3 * k + 1) / 2
+					if pentagonal1 > m then return sum
+					sum += (if k % 2 == 0 then -1 else 1) * (loop(m - pentagonal1) + loop(m - pentagonal2))
+					k += 1
+				cache(m) = sum
+				sum
 		}
 		
-		count(sum, 1)
+		loop(n)
 	}
 	
-	val answer = (LazyList from 1).find(coinPartitionCounts(_) % 1_000_000 == 0).get
-	println(answer) // Never finished. TODO: Make it fast.
+	val answer = (LazyList from 1).find(partition(_) % 1_000_000 == 0)
+	println(answer) // Still way to slow. TODO: Make it fast.
 }
