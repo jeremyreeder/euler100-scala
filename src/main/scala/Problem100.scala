ff728b2fa474deb517f2cbff1a3a1d7c9e1d1d14
Jeremy Reeder <jeremy.reeder@pm.me>
2023-04-25 15:20:23 -0600

    Still scratching my head.

--- src/main/scala/Problem100.scala
+++ src/main/scala/Problem100.scala
@@ -8,22 +8,33 @@ eighty-five blue discs and thirty-five red discs.
 By finding the first arrangement to contain over 10^12 = 1,000,000,000,000 discs in total, determine the number of blue
 discs that the box would contain.
 */
-import math.sqrt
-
 object Problem100 extends App {
 	// b/d * (b-1)/(d-1) = 1/2
 	// d/b ~= sqrt(2); d/b < sqrt(2); (d-1)/(b-1) > sqrt(2)
-	// Either b or d-b seems to always be one less than a square.
 	
-	val MinD = 1_000_000_000_000L
+	extension (n: BigInt) {
+		def sqrt =
+			val d = BigDecimal(n)
+			var a = BigDecimal(1)
+			var b = d
+			while b >= a do
+				val mid = (a + b) / 2
+				if mid * mid > d then b = mid - 0.000_001
+				else a = mid + 0.000_001
+			b
+	}
+	
+	val MinD = BigInt(1_000_000_000_000L)
 	
 	def solutions =
 		for {
 			d <- LazyList.range(MinD, MinD + Int.MaxValue)
-			b = (sqrt(2 * d * d - 2 * d + 1) + 1) / 2
-				if b == b.floor
-		} yield (b.toLong, d)
+			b = ((2 * d * d - 2 * d + 1).sqrt + 1) / 2
+				if b.isWhole
+		} yield (b, d)
 	
-	def answer = solutions.head
-	println(answer) // 1_168_070_918 is wrong.
+	println(BigInt(81).sqrt)
+	solutions.take(14).foreach(println)
+	//println(answer) // 1_168_070_918 is wrong.
+	// TODO: Solve this problem.
 }
